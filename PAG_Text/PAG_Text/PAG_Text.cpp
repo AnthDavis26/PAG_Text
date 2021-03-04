d8230a734b52b8e885731c924c39d9034df74d56
AnthDavis26 <42490176+AnthDavis26@users.noreply.github.com>
2021-03-04 09:44:27 -0800

    Test byte set function for files

--- PAG_Text/PAG_Text/PAG_Text.cpp
+++ PAG_Text/PAG_Text/PAG_Text.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
+#include <filesystem>
+#include <fstream>
+
+const std::string SAVE_PATH = ".\\Saves\\";
+
+// Prototypes
+void SetByteAt(std::string fullpath, int byte, int addr);
+
+void SetByteAt(std::string fullpath, int byte, int addr)
+{
+    // Read and write out to file
+    std::ofstream file(fullpath, std::ios::in | std::ios::out | std::ios::binary);
+
+    file.seekp(addr);
+    file.put(byte);
+
+    file.close();
+}
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    // Create a test save file
+    SetByteAt(SAVE_PATH + "SAV", 3, 0x022);
 }
