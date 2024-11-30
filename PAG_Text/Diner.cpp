9581cbc646b57e03eb4d81a5d467477ec15fc830
Anthony Davis <anthony.m.davis.26@gmail.com>
2024-11-29 19:48:20 -0500

    Implement diner owner punch / kill logic

--- PAG_Text/Diner.cpp
+++ PAG_Text/Diner.cpp
@@ -7,17 +7,64 @@
 
 void Diner::Start() {
 	Prompter::SceneTitle("Diner");
+	Prompter::Print("You're inside the local diner. It doesn't even have a proper name.\n");
+
+	GameCharacter& GCGeorge = GameCharacters::GEORGE;
+	GameCharacter& GCDinerOwner = GameCharacters::DINER_OWNER;
+
+	if (SaveManager::KilledDinerOwnerThenLeft()) {
+		SaveManager::SetKilledDinerOwnerThenLeft(false);
+		GCDinerOwner.SetFullHealth();
+		SaveManager::SetKilledDinerOwner(false);
+		GCGeorge.Speak("Wait, didn't I kill you?");
+		GCDinerOwner.Speak("Yes, but I'm better now!");
+		GCGeorge.Speak("That's such a relief! I'm so glad you're ok!");
+		GCDinerOwner.Speak("Thank you, so am I!");
+	} else {
+		GCGeorge.Speak("Too bad they don't have food here, just condiments.");
+	}
 
 	if (Rules::CAN_SAVE_AT_DINER) {
 		SaveManager::SetCurrentLocation(LocationKeys::DINER);
 	}
 
-	Prompter::Print("You're inside the local diner. It doesn't even have a proper name.\n");
-	GameCharacters::GEORGE.Speak("Too bad they only serve condiments here.");
-	Prompter::Print("1. Leave the diner");
+	Prompter::Print(
+		"1. Leave the diner\n"
+		"2. Punch the diner owner"
+	);
+
 	Prompter::ShowDefaultControls();
 
 	Prompter::PromptUntilValidCommand(
-		std::make_pair('1', Navigator::GoToHomeStreet)
+		CMD('1', [] {
+			if (SaveManager::KilledDinerOwner()) {
+				SaveManager::SetKilledDinerOwnerThenLeft(true);
+			}
+
+			Navigator::GoToHomeStreet(); }),
+		CMD('2', [] {
+			Prompter::Print("You punched the diner owner.");
+			Prompter::Print("");
+
+			GameCharacter& GCGeorge = GameCharacters::GEORGE;
+			GameCharacter& GCDinerOwner = GameCharacters::DINER_OWNER;
+
+			if (!SaveManager::KilledDinerOwner()) {
+				GCGeorge.HitActor(GCDinerOwner, 99);			
+				
+				if (GCDinerOwner.GetHealth() > 0) {
+					GCDinerOwner.Speak("Ow, you hit me.");
+					GCGeorge.Speak("Sorry.");
+				} else {
+					GCDinerOwner.Speak("Ow, you killed me.");
+					SaveManager::SetKilledDinerOwner(true);
+					GCGeorge.Speak("Good.");
+				}
+			} else {
+				GCGeorge.Speak("Yep, still dead.");
+			}
+
+			Diner::Start(); 
+		})
 	);
 }
