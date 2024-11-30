fb5aad293a4e134632bec6fbfbb4db7f26fca141
Anthony Davis <anthony.m.davis.26@gmail.com>
2024-11-29 19:55:54 -0500

    Add savemanager debug check

--- PAG_Text/PAG_Text.cpp
+++ PAG_Text/PAG_Text.cpp
@@ -6,11 +6,11 @@
 int main() {
 	Navigator::InitLocations();
 	SaveManager::InitSaveFiles();
+	SaveManager::DEBUG_CheckAllOverlaps();
 	
 	if (SaveManager::StartedGame()) {
 		Navigator::GoToMainMenu();
-	}
-	else {
+	} else {
 		MainMenu::NewGame();
 	}
 
